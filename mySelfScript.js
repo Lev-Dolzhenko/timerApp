5b8b7c28d2b2f5b6dfa8904332830792048d956d
Leo <workspace2707@yandex.ru>
2023-11-15 22:11:45 +0300

    created timerApp

---
+++ mySelfScript.js
@@ -0,0 +1,45 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let $timeStart = document.querySelector('.time-start');
+    let $timeEnd = document.querySelector('.time-end');
+    let $timeButton = document.querySelector('.time-button');
+    let $days = document.querySelector('.timer__days');
+    let $hours = document.querySelector('.timer__hours');
+    let $minutes = document.querySelector('.timer__minutes');
+    let $seconds = document.querySelector('.timer__seconds');
+
+
+    $timeButton.addEventListener('click', countdownTimer);
+    $timeButton.addEventListener('click', timer);
+    let timerId = null;
+
+    function declensionNum(num, words) {
+        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+
+    function countdownTimer() {
+        const deadline = new Date($timeEnd.value);
+        const diff = deadline - new Date();
+        if (diff <= 0) {
+            clearInterval(timerId);
+        }
+        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+        const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+        const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+        $days.textContent = days < 10 ? '0' + days : days;
+        $hours.textContent = hours < 10 ? '0' + hours : hours;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+        $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+        $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+        $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+        $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+
+    function timer() {
+        timerId = setInterval(countdownTimer, 1000);
+    }
+})
